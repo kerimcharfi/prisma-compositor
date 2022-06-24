fa1e2c66bd0fca53f43bb40060eafdf7697752dd
Kerim <kerimcharfi@outlook.de>
2022-06-24 23:04:08 +0200

    add newline

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const [inputPath, outputFile] = process.argv.slice(2);
 let fileContent = ""
 fs.readdirSync(inputPath).forEach(fileName => {
     if (fileName.search(".prisma") !== -1){
-        fileContent += fs.readFileSync(path.join(inputPath, fileName), "utf-8")
+        fileContent += fs.readFileSync(path.join(inputPath, fileName), "utf-8") + "\n"
+
     }
 });
 
